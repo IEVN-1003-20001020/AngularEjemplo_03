392b2d04bb6f5d025596e29942317cb8aab754d3
Milton Ramirez <ramitezmilton@gmail.com>
2023-09-20 19:36:51 -0600

    20/09/23 MAVR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,29 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
+  
 })
+
+
 export class AppComponent {
   title = 'AngularEjemplo_03';
+
+  duplicarValor(valor:number):number{
+    return valor*2
+  }
+
+
+  sumar(valor2:number):number{
+    return valor2 + 100
+  }
+
+
+  pelicula={
+    titulo:'Spider-man',
+    fechaLanzamiento:new Date(),
+  }
+
 }
+
+
