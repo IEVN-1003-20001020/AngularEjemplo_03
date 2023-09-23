1e0ef654e8cbee8957f8abc84d94e6894707e68a
Milton Ramirez <ramitezmilton@gmail.com>
2023-09-22 21:08:35 -0600

    22/09/2023 -- MAVR

---
+++ src/app/productos/iproductos.ts
@@ -0,0 +1,9 @@
+export interface IProductos {
+    "Modelo":string,
+    "Descripcion":string,
+    "Anio":string,
+    "precio":number,
+    "Marca":string,
+    "Color":string,
+    "ImagenUrl":string
+}
