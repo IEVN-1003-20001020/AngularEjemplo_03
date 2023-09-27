e40105b48d067c0878b87b60344f3ad5bb7e0c79
Milton Ramirez <ramitezmilton@gmail.com>
2023-09-26 21:03:26 -0600

    26/09/23 MAVR

---
+++ src/app/calculos/operas-bas/operas-bas.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operas-bas',
+  templateUrl: './operas-bas.component.html',
+  styleUrls: ['./operas-bas.component.css']
+})
+export class OperasBasComponent {
+
+  num1:string='';
+  num2:string='';
+  resultado:number=0;
+  radio1:string='';
+
+  
+  
+  calcular(){
+
+    switch (this.radio1) {
+
+
+      case 'suma':
+        this.resultado=parseInt(this.num1) + parseInt(this.num2)
+        break;
+      case 'resta':
+        this.resultado=parseInt(this.num1) - parseInt(this.num2)
+        break;
+      case 'mult':
+        this.resultado=parseInt(this.num1) * parseInt(this.num2)
+        break;
+      case 'divid':
+        this.resultado=parseInt(this.num1) / parseInt(this.num2)
+        break;
+        
+      default:
+        console.log('holi crayoli');
+        
+        break;
+    }
+
+    
+
+  }
+
+
+  
+
+
+
+
+  /*sumar():void{
+    this.resultado=parseInt(this.num1) + parseInt(this.num2)
+  }
+
+  restar():void{
+    this.resultado=parseInt(this.num1) - parseInt(this.num2)
+  }
+
+  mult():void{
+    this.resultado=parseInt(this.num1) * parseInt(this.num2)
+  }
+
+  divid():void{
+    this.resultado=parseInt(this.num1) / parseInt(this.num2)
+  } */
+
+}
